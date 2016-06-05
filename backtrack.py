91b59d9f4a5214476a2e49afc99e2997778ad582
Reed Dunkle <reeddunkle@gmail.com>
2016-06-05 16:06:17 -0400

    Began refactoring process

--- backtrack.py
+++ backtrack.py
@@ -3,8 +3,8 @@ import time
 from random import randint
 os.system('cls' if os.name == 'nt' else 'clear')
 
-def possible_directions(row, col):
-    '''Returns the moves that are legal from the given coordinates.'''
+def possible_directions(row, col, obstacles):
+    """Returns the moves that are legal from the given coordinates."""
 
     dirs = []
     if row == 0 and col == 0:
@@ -16,32 +16,32 @@ def possible_directions(row, col):
     return dirs
 
 
-def find_path(row, col):
-    '''The backtrack algorithm -- returns the robots path.'''
+def find_path(row, col, destination_row, destination_column, obstacles):
+    """The backtrack algorithm -- returns the robot's path."""
 
-    if (DEST_R == 0 and DEST_C == 1) or (DEST_R == 1 and DEST_C == 0):
+    if (destination_row == 0 and destination_column == 1) or (destination_row == 1 and destination_column == 0):
         return [(0, 0), (row, col)]
     else:
-        for coordinance in possible_directions(row, col):
+        for coordinance in possible_directions(row, col, obstacles):
             r, c = coordinance
             if r == 0 and c == 0:
                 return [(0, 0)]
 
-            solution = find_path(r, c)
+            solution = find_path(r, c, destination_row, destination_column, obstacles)
             if solution:
                 final = solution + [(r, c)]
-                if row == DEST_R and col == DEST_C:
+                if row == destination_row and col == destination_column:
                     final += [(row, col)]
                 return final
 
 
-def create_board(row, col):
+def create_board(number_of_rows, number_of_columns, obstacles):
     '''Returns an array representation of the board.'''
 
     board = []
-    for r in range(row + 1):
+    for r in range(number_of_rows + 1):
         board.append([])
-        for c in range(col + 1):
+        for c in range(number_of_columns + 1):
             if (r, c) in obstacles:
                 board[r].append("[X]")
             else:
@@ -50,7 +50,7 @@ def create_board(row, col):
 
 
 def convert_coords_english(path):
-    """Writes the English directions of the robot's path"""
+    """Writes the English directions of the robot's path."""
 
     path_english = []
     for i in range(0, -1 + len(path)):
@@ -65,10 +65,10 @@ def convert_coords_english(path):
     return path_english
 
 
-def draw_path_to_board(board, path):
-    """Draws the robot's path to the board"""
+def draw_path_to_board(board, path, destination_row, destination_column):
+    """Draws the robot's path to the board."""
 
-    if DEST_R == 0 and DEST_C == 0:
+    if destination_row == 0 and destination_column == 0:
         return ["[+]"]
 
     if path:
@@ -91,12 +91,13 @@ def draw_path_to_board(board, path):
             elif cur_move == "Down":
                 board[r][c] = "[|]"
 
-        board[DEST_R][DEST_C] = "[*]"
+        board[destination_row][destination_column] = "[*]"
     return board
 
 
 def convert_board_string(board):
     """Returns a string representation of the board."""
+
     board_string = ""
     for row in board:
         row_string = ""
@@ -105,92 +106,112 @@ def convert_board_string(board):
         board_string += row_string + "\n"
     return board_string
 
+
 def print_introduction():
+    """Prints the game's introduction messages."""
+
     print("-" * 15)
     print("Welcome to the Robot Game!")
     print("Your robot starts in the upper left corner")
     print("of the game board, at coordinates (0,0)\n")
     print("She can only move to the RIGHT and DOWN.\n")
 
-def is_int(s):
-    try:
-        int(s)
-        return True
-    except ValueError:
-        return False
 
 def get_destination():
-    print("\nWhat are the coordinates of your robot's destination?")
-    print("Example: 2,2")
-    coord = input("> ")
-    int_coord = []
-    for c in coord:
-        if is_int(c) and len(int_coord) < 2:  # Grabs the first two integers
-            int_coord.append(c)
-    try:
-        r, c = int_coord
-    except (ValueError, UnboundLocalError):
-        print("Sorry. You must enter the destination in the form Row,Column")
-        get_destination()
+    """Gets coordinates from the user for the robot's destination"""
 
-    return (int(r), int(c))
+    prompt = "What are the coordinates of your robot's destination?\nExample: 2,2\n> "
+    error_msg = "Sorry. You must enter the destination in the form Row,Column."
+    test = lambda s: len([c for c in s if c.isdigit()]) > 1
+
+    user_input = validate_user_input(prompt, error_msg, test)
+    r, c = [c for c in user_input if c.isdigit()][:2]
+    r = int(r)
+    c = int(c)
+
+    return (r, c)
 
 
-def message():
+def play_game(path, board_string, board_path_string):
+    """Displays the game's process to the user."""
+
     print("\nOkay. Right now there are some obstacles...")
     print("Those squares are marked with an X.\n")
-    print(BOARD_STRING)
+    print(board_string)
     for x in range(2):
         if x == 0:
             m = "Looking for a path..."
             print(m, end="\r")
-            time.sleep(2.5)
-        elif PATH:
+            ###time.sleep(2.5)
+        elif path:
             m = "Looking for a path...Success!!!"
             m += "\nCheck it out:\n"
             print(m, end="\r")
-            print(BOARD_PATH_STRING)
+            print(board_path_string)
         else:
             m = "Looking for a path...Darn!"
             m += "\nThere doesn't seem to be a path through the obstacles!\n"
             print(m, end="\r")
+            print("Such ill-placed obstacles.")
 
-    if PATH:
-        print("\nPress ENTER to see the path's coordinates:")
-        display = input()
-        print(PATH)
-    else:
-        print("Such ill-placed obstacles.")
 
-def get_obstacles():
+def validate_user_input(prompt, error_msg, test):
+    """
+    Keeps prompting the user for a valid input. <test> is a function that returns
+    a Boolean value, depending on whether or not the input is valid.
+    """
+
+    user_input = input(prompt)
+
+    while not test(user_input):
+        if not test(user_input):
+            print(error_msg)
+
+        user_input = input(prompt)
+
+    return user_input
+
+
+def generate_obstacles(destination_row, destination_column):
+    """Prompts the user to decide how many obstacles to generate."""
+
+    # destination_x are coordinates, not size, thus we need to +1
+    # We subtract 2 because src and trg are not allowed to have obstacles.
+    max_obstacles = (destination_row + 1) * (destination_column + 1) - 2
+
+    prompt = "Please enter a number of random obstacles:\n> "
+    error_msg = "Sorry, for a board of this size\nyou can only have {} obstacles.".format(max_obstacles)
+
+    num_obstacles = validate_user_input(prompt, error_msg, lambda s: s.isdigit() and int(s) < max_obstacles)
+    num_obstacles = int(num_obstacles)
+
+    current_obstacles = 0
     obstacles = []
-    max_obs = (DEST_R + 1) * (DEST_C + 1) - 2
-    print("Would you like to add in random obstacles?")
-    num_obs = input("Enter a number: ")
-    n = 0
-    while (n < int(num_obs)):
-        r = randint(0, DEST_R)
-        c = randint(0, DEST_C)
-        if (r == 0 and c == 0) or (r == DEST_R and c == DEST_C):
-            n -= 1
+    while (current_obstacles < num_obstacles):
+        r = randint(0, destination_row)
+        c = randint(0, destination_column)
+
+        # Avoid placing obstacles in src and trg
+        if (r == 0 and c == 0) or (r == destination_row and c == destination_column):
             continue
-        elif (r, c) not in obstacles:
-            obstacles.append((r, c))
+
+        elif (r, c) in obstacles:
+            continue
+
         else:
-            n -= 1
-        n += 1
-        if len(obstacles) >= max_obs:
-            return obstacles
+            obstacles.append((r, c))
+            current_obstacles += 1
+
     return obstacles
 
 
-print_introduction()
-DEST_R, DEST_C = get_destination()
-obstacles = get_obstacles()
-
-GAME_BOARD = create_board(DEST_R, DEST_C)
-PATH = find_path(DEST_R, DEST_C)
-BOARD_STRING = convert_board_string(GAME_BOARD)
-BOARD_WITH_PATH = draw_path_to_board(GAME_BOARD, PATH)
-BOARD_PATH_STRING = convert_board_string(BOARD_WITH_PATH)
-message()
+if __name__ == '__main__':
+    print_introduction()
+    destination_row, destination_column = get_destination()
+    obstacles = generate_obstacles(destination_row, destination_column)
+    game_board = create_board(destination_row, destination_column, obstacles)
+    backtrack_path = find_path(destination_row, destination_column, destination_row, destination_column, obstacles)
+    board_string = convert_board_string(game_board)
+    board_with_path = draw_path_to_board(game_board, backtrack_path, destination_row, destination_column)
+    board_path_string = convert_board_string(board_with_path)
+    play_game(backtrack_path, board_string, board_path_string)
